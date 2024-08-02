70bd19582f406f35de8a0290f97684527e63fcdc
Shubham Yadav <shubh08am@gmail.com>
2024-08-02 12:52:39 +0530

    Update HeapSort.cpp

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -1,45 +1,40 @@
 #include<bits/stdc++.h>
-using namespace std ; 
+using namespace std; 
 
-void heapify(int arr[] , int n , int i){
-   int largest = i ; 
-   int left = 2*i+1;
-   int right = 2*i+2; 
-
-   if(left<n && arr[largest]<arr[left]){
-    largest=left;
-   }
-   if(right<n && arr[largest]<arr[right]){
-    largest=right;
-   }
-
-   //swapping 
-   if(largest!=i){
-         swap(arr[largest],arr[i]);
-         heapify(arr,n,largest);
-   }
+  //using max heap
+void heapify(vector<int>&arr,int n,int i){
+    int largest=i;
+    int left=2*i+1,right=2*i+2; 
+    if(left<n && arr[left]>arr[largest]){
+        largest=left;
+    }
+    if(right<n && arr[right]>arr[largest]){
+        largest=right;
+    }
+    if(largest!=i){
+        swap(arr[largest],arr[i]) ; 
+        heapify(arr,n,largest); 
+    }
 }
-
-void heapsort(int arr[] , int n ){
-   //from non-leaf nodes 
-   for(int i=n/2-1;i>=0;--i){
+void heapsort(vector<int>&arr,int n){
+    //first make heap :- max heap
+    //non leaf nodes in cbt from n/2-1 to 0 
+ for(int i=n/2-1;i>=0;i--){
     heapify(arr,n,i);
-   }
-   
-   for(int i=n-1;i>0;i--){
-    swap(arr[i],arr[0]);
-   heapify(arr,i,0);
-   }
+ }
 
+ //now remove root node from max heap and bring smallest element on root node and heapify again 
+ for(int i=n-1;i>0;i--){
+    swap(arr[i],arr[0]); 
+    heapify(arr,i,0);
+ }
 }
-
 int main(){
-    int n ; cin>>n ; 
-    int arr[n] ; 
+    int n;cin>>n; 
+    vector<int>arr(n); 
     for(int i=0;i<n;i++) cin>>arr[i] ; 
+    
+    heapsort(arr,n); 
 
-    heapsort(arr,n);
-    cout<<"After-HeapSort\n";
     for(int i=0;i<n;i++) cout<<arr[i]<<" ";
-
-}
+ }
